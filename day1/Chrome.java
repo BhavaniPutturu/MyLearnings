9e39171a1e5a98967540168381a3f64ca5cb2c30
BhavaniPutturu <bhavanicsuk@gmail.com>
2025-03-01 18:15:41 +0000

    day1

---
+++ day1/Chrome.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float version = 91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortcutKey = 'C';
+		System.out.println(developer + " " + version + 
+				" was released in the year " + releaseYear + " with the shortcut key " + 
+				shortcutKey + ".\nThe value of 'isBeta' variable is " + isBeta + ".");
+	}
+
+}
